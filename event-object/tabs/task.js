cf85debc492482795b305c776cd2609a5cee6797
Andrew Fedorov <andrewfedorov@MacBook-Pro-Andrew.local>
2023-01-26 16:35:27 +0300

    all done

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -3,14 +3,17 @@ console.log(tab, ' tab');
 const tabContent = document.querySelectorAll('.tab__content')
 console.log(tabContent, ' content');
 
-tab.forEach((el, index) => el.onclick = (event) => {
-  let current = event.target
-  tab.forEach(e => {
-    if (e !== current){
-      e.classList.remove('tab_active')
-    
-    }
+function removeActive() {
+  tab.forEach(el => {
+    el.classList.remove('tab_active')
   })
+  tabContent.forEach(el => {
+    el.classList.remove('tab__content_active')
+  })
+}
+
+tab.forEach((el, index) => el.onclick = () => {
+  removeActive()
   el.classList.add('tab_active')
   tabContent[index].classList.toggle('tab__content_active')
 })
