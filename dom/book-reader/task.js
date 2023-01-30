be896944261b2b617b4fa31ee24075116572ceed
Andrew Fedorov <andrewfedorov@MacBook-Pro-Andrew.local>
2023-01-30 19:01:41 +0300

    all done

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,5 +1,9 @@
 const fontsCollection = document.querySelectorAll('.font-size');
+const textColorCollection = document.querySelector('.book__control_color').querySelectorAll('.color')
+const bgColorCollection = document.querySelector('.book__control_background').querySelectorAll('.color')
 const bookContent = document.querySelector('.book__content');
+let bgColor = '';
+let textColor = ''
 
 fontsCollection.forEach(el => {
   el.onclick = (event) => {
@@ -19,6 +23,63 @@ fontsCollection.forEach(el => {
       bookContent.classList.add('book_fs-small')
     } else {
       bookContent.className = 'book__content'
+    }
+  }
+})
+
+textColorCollection.forEach(el => {
+  el.onclick = (event) => {
+    event.preventDefault()
+    const target = event.target
+    target.classList.add('color_active')
+    textColorCollection.forEach((e) => {
+      if (e != target) {
+        e.classList.remove('color_active')
+      }
+    })
+    const textColor = target.dataset.textColor;
+
+    if (textColor === 'black') {
+      bookContent.classList.remove('book_color-whitesmoke')
+      bookContent.classList.remove('book_color-gray')
+      bookContent.classList.add('book_color-black')
+    } else if (textColor === 'gray') {
+      bookContent.classList.remove('book_color-whitesmoke')
+      bookContent.classList.remove('book_color-black')
+      bookContent.classList.add('book_color-gray')
+    } else {
+      bookContent.classList.remove('book_color-black')
+      bookContent.classList.remove('book_color-gray')
+      bookContent.classList.add('book_color-whitesmoke')
+    }
+  }
+})
+
+
+bgColorCollection.forEach(el => {
+  el.onclick = (event) => {
+    event.preventDefault()
+    const target = event.target
+    target.classList.add('color_active')
+    bgColorCollection.forEach((e) => {
+      if (e != target) {
+        e.classList.remove('color_active')
+      }
+    })
+    const bgColor = target.dataset.bgColor;
+
+    if (bgColor === 'black') {
+      bookContent.classList.remove('bg_color_gray')
+      bookContent.classList.remove('bg_color_white')
+      bookContent.classList.add('bg_color_black')
+    } else if (bgColor === 'gray') {
+      bookContent.classList.remove('bg_color_black')
+      bookContent.classList.remove('bg_color_white')
+      bookContent.classList.add('bg_color_gray')
+    } else {
+      bookContent.classList.remove('bg_color_gray')
+      bookContent.classList.remove('bg_color_black')
+      bookContent.classList.add('bg_color_white')
     }
   }
 })
