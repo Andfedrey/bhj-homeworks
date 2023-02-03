b4ebfbf2ba14c44ae86dd8ca72fd741f28276281
Andrew Fedorov <andrewfedorov@MacBook-Pro-Andrew.local>
2023-02-03 07:19:36 +0300

    all done

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -5,7 +5,7 @@ const plus = document.querySelectorAll('.product__quantity-control_inc');
 
 min.forEach(el => {
   el.onclick = ({ target }) => {
-    target.nextElementSibling.textContent > 0 ? target.nextElementSibling.textContent-- : 0
+    target.nextElementSibling.textContent > 1 ? target.nextElementSibling.textContent-- : 1
   }
 })
 
@@ -22,18 +22,19 @@ addProduct.forEach(el => {
     let count = parentTarget.querySelector('.product__quantity-value').textContent.trim();
     let dataId = parentTarget.dataset.id;
     let checkId = document.getElementById(`${dataId}`)
-    if (count > 0 && checkId === null) {
+    if (checkId === null) {
       cart.innerHTML += `
       <div class="cart__product" id=${dataId}>
       <img class="cart__product-image" src=${img}>
       <div class="cart__product-count">${count}</div>
       </div>
       `
-    } else if (count > 0 && checkId !== null) {
+    } else if (checkId !== null) {
       //Можно это как то более элегантно написать?
       let a = checkId.querySelector('.cart__product-count').innerHTML;
       let sum = +a + +count;
       checkId.querySelector('.cart__product-count').innerHTML = sum
     }
+    parentTarget.querySelector('.product__quantity-value').textContent = 1
   }
 })
