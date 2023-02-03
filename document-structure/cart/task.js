aacc2cee923b58d394a475ac14bee021fa2baaee
Andrew Fedorov <andrewfedorov@MacBook-Pro-Andrew.local>
2023-02-03 07:15:15 +0300

    all done

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -21,13 +21,19 @@ addProduct.forEach(el => {
     let img = parentTarget.querySelector('.product__image').src;
     let count = parentTarget.querySelector('.product__quantity-value').textContent.trim();
     let dataId = parentTarget.dataset.id;
-    if (count > 0) {
+    let checkId = document.getElementById(`${dataId}`)
+    if (count > 0 && checkId === null) {
       cart.innerHTML += `
-        <div class="cart__product" data-id=${dataId}>
-          <img class="cart__product-image" src=${img}>
-          <div class="cart__product-count">${count}</div>
-        </div>
-    `
+      <div class="cart__product" id=${dataId}>
+      <img class="cart__product-image" src=${img}>
+      <div class="cart__product-count">${count}</div>
+      </div>
+      `
+    } else if (count > 0 && checkId !== null) {
+      //Можно это как то более элегантно написать?
+      let a = checkId.querySelector('.cart__product-count').innerHTML;
+      let sum = +a + +count;
+      checkId.querySelector('.cart__product-count').innerHTML = sum
     }
   }
 })
