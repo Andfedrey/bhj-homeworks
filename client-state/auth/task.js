c0448a2a1116d6e70470fe5c8b940c8552d48940
Andrew Fedorov <andrewfedorov@MacBook-Pro-Andrew.local>
2023-02-07 06:46:21 +0300

    all done;

---
+++ client-state/auth/task.js
@@ -0,0 +1,24 @@
+const control = document.querySelectorAll('.control');
+const form = document.getElementById('signin__form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const xhr = new XMLHttpRequest();
+  let info;
+
+  control.forEach((el) => {
+    info = { ...info, [el.name]: el.value }
+  })
+
+  xhr.open("POST", " https://students.netoservices.ru/nestjs-backend/auth")
+  xhr.send(JSON.stringify(info));
+  checkRequest(xhr)
+})
+
+function checkRequest(req) {
+  if (req.status === 200) {
+    document.querySelector('#signin').className = 'signin';
+  } else {
+    alert('Неверный логин/пароль')
+  }
+}
---
+++ client-state/popup/task.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector('.modal')
+
+
+function getSesionModal() {
+  if (!sessionStorage.length) {
+    sessionStorage.setItem('flag', 'active');
+    modal.classList.add('modal_active')
+
+    document.querySelector('.modal__close').onclick = () => {
+      modal.className = 'modal'
+    } 
+  }
+}
+
+getSesionModal()
+
+
+
+
+
