153303a3203a3b7a9a92838fe107470e4a16c287
Andrew Fedorov <andrewfedorov@MacBook-Pro-Andrew.local>
2023-02-07 07:46:45 +0300

    all done

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,25 +1,24 @@
 const control = document.querySelectorAll('.control');
 const form = document.getElementById('signin__form');
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
+function sendData(payload) {
   const xhr = new XMLHttpRequest();
-  let info;
+  xhr.open("POST", 'https://students.netoservices.ru/nestjs-backend/auth')
+  xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  xhr.send(JSON.stringify(payload));
+}
 
+function formSend(event) {
+  event.preventDefault()
+  let info;
   control.forEach((el) => {
     info = { ...info, [el.name]: el.value }
   })
-
-  xhr.open("POST", " https://students.netoservices.ru/nestjs-backend/auth")
-  xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-  xhr.send(JSON.stringify(info));
-  checkRequest(xhr)
-})
-
-function checkRequest(req) {
-  if (req.status >= 200 ) {
-    document.querySelector('#signin').className = 'signin';
-  } else {
-    alert('Неверный логин/пароль')
-  }
+  sendData(info)
 }
+
+form.addEventListener('submit', formSend)
+
+
+
+
