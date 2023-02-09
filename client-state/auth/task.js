1a7b9bac40a3cf8f4761b50e96f10f400c12d2e8
Andrew Fedorov <andrewfedorov@MacBook-Pro-Andrew.local>
2023-02-09 07:17:56 +0300

    all done

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,11 +1,19 @@
 const control = document.querySelectorAll('.control');
 const form = document.getElementById('signin__form');
+const welcomeUser = document.querySelector('.welcome');
+const xhr = new XMLHttpRequest();
 
 function sendData(payload) {
-  const xhr = new XMLHttpRequest();
   xhr.open("POST", 'https://students.netoservices.ru/nestjs-backend/auth')
   xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  xhr.responseType = 'json'
   xhr.send(JSON.stringify(payload));
+}
+
+xhr.onload = function() {
+    welcomeUser.classList.add('welcome_active')
+    let resposeObj = xhr.response;
+    document.getElementById('user_id').outerText = resposeObj.user_id;
 }
 
 function formSend(event) {
